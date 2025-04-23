62466ef38577a114fb4132126dc8c82bfa094868
N Walk <MoonSupreme1927@gmail.com>
2025-04-22 19:54:38 -0500

    Updating Login.js

--- frontend/src/UserDetail.js
+++ frontend/src/UserDetail.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import axios from 'axios';
 import { useParams, useNavigate } from 'react-router-dom';
+import axios from 'axios';
+
 
 
 function UserDetail() {
