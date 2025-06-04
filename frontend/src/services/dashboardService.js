bc70a07d0eeaa9e0b43fdeb451baa1f74e110b03
N Walk <MoonSupreme1927@gmail.com>
2025-06-04 05:46:24 -0500

    Added services

---
+++ frontend/src/services/dashboardService.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+const API_BASE = 'https://user-lookup-app.onrender.com';
+
+export const fetchCurrentBook = () => axios.get(`${API_BASE}/dashboard/book-of-the-month`);
+
+export const updateCurrentBook = (bookData) =>
+  axios.post(`${API_BASE}/dashboard/book-of-the-month`, bookData);
+
+export const fetchChartData = () => axios.get(`${API_BASE}/dashboard/charts`);
+export const fetchUserData = (token) =>
+  axios.get(`${API_BASE}/dashboard`, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+  
