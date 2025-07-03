51aded62b285c63ca762870e7453c2f6875490e1
N Walk <MoonSupreme1927@gmail.com>
2025-07-02 20:26:30 -0500

    updated bookadminbtton

--- frontend/src/AdminDashboard.js
+++ frontend/src/AdminDashboard.js
@@ -7,7 +7,7 @@ import { useNavigate } from 'react-router-dom';
 // It includes features for promoting users to admin, deleting users, and displaying user statistics.
 //import './styles.css'; // Import additional styles if needed
 //import './darkMode.css'; // Import dark mode styles if applicable
-import './AdminBookFormm.js'; // Import the admin book form component if needed
+import './AdminBookForm.js'; // Import the admin book form component if needed
 const AdminDashboard = () => {
   const [users, setUsers] = useState([]);
   const [filteredUsers, setFilteredUsers] = useState([]);
